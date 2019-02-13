eae7084f13095796706833ffe2aaee210f5d18aa
francuz08011990 <francuz08011990@gmail.com>
2019-02-13 13:43:50 +0200

    Added detail list of employees

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import all_employees
+from .views import all_employees, detail_list
 
 urlpatterns = [
-    path('', all_employees, name='all_employees')
+    path('', all_employees, name='all_employees'),
+    path('detail_list/', detail_list, name='detail_list')
 ]
--- app/views.py
+++ app/views.py
@@ -6,3 +6,9 @@ def all_employees(request):
     employees_list = Profile.objects.filter(chief=None)
     data = {'employees': employees_list}
     return render(request, 'index.html', data)
+
+
+def detail_list(request):
+    employees = Profile.objects.all()
+    data = {'employees': employees}
+    return render(request, 'detail_list.html', data)
